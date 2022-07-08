5bcc8be60781957c14f96c193b9c5a1251efd2af
WuShark1023 <st97405@gmail.com>
2022-07-08 21:19:03 +0800

    Create Tank Game Project

---
+++ TankGame/Form1.cs
@@ -0,0 +1,13 @@
+namespace TankGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(500, 250);
+        }
+    }
+}
