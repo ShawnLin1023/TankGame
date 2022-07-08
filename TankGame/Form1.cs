a5ce97e477a4210126c2995d029a20cc4dcac1d8
WuShark1023 <st97405@gmail.com>
2022-07-08 22:55:26 +0800

    Add Graphics Feature

--- TankGame/Form1.cs
+++ TankGame/Form1.cs
@@ -6,8 +6,19 @@ namespace TankGame
         {
             InitializeComponent();
 
-            this.StartPosition = FormStartPosition.Manual;
-            this.Location = new Point(500, 250);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            /*this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(500, 250);*/
+        }
+
+        //在視窗建立完成會呼叫一次Paint，重新繪製完成也會呼叫一次
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //練習使用畫布+筆繪製出線與字串
+            Graphics g = this.CreateGraphics();
+            Pen p = new Pen(Color.Black);
+            g.DrawLine(p, new Point(0, 0), new Point(100, 100));
+            g.DrawString("WuShark", new Font("Arial", 20), new SolidBrush(Color.Red), new Point(100, 100));
         }
     }
 }
